d7c5cf26f624116e665cc60546880ec54af341a9
Hào Hay Ke <134072533+MaiTheHao@users.noreply.github.com>
2024-08-02 22:32:15 +0700

    update

--- BaoProject1.cpp
+++ BaoProject1.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   string x, y, z;
-   x=y=z = "hello";
-   string A;
-   A = x[1] + y[2] + z[0];
-   cout << "A = " << A << endl;
-   return 0;
+int main()
+{
+    string x, y, z, A;
+    x = y = z = "hello";
+    A = x.substr(1, 1) + y.substr(2, 1) + z.substr(0, 1);
+    cout << "A = " << A << endl;
+    return 0;
 }
