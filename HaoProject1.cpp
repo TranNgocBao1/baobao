4958b8580f237e0b620fd19c2ab73cb18ba5da7e
Hào Hay Ke <134072533+MaiTheHao@users.noreply.github.com>
2024-08-04 03:41:00 +0700

    setup var

--- HaoProject1.cpp
+++ HaoProject1.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
+#include <unistd.h>
+#include <ctime>
 using namespace std;
 
 enum LEVEL
@@ -21,10 +24,18 @@ struct User {
 
 int main()
 {
+    srand(time(0));
     User Kien, Tam;
     Kien = {"Tran Manh Kien"};
     Tam = {"Nguyen Le Thi Tam Cam Tam"};
 
+    User listUsers[] = {Kien, Tam};
+
+    while(true){
+        bool round = rand() % 2;
+        cout << listUsers[round].name <<" do you love " << listUsers[!round].name << "?" << endl;
+        sleep(2);
+    }
 
     return 0;
 }
