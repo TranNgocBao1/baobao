082fd1c9cca334de1380fc8b1712bbce95210362
Hào Hay Ke <134072533+MaiTheHao@users.noreply.github.com>
2024-08-04 18:27:06 +0700

    complete

--- HaoProject1.cpp
+++ HaoProject1.cpp
@@ -7,20 +7,73 @@ using namespace std;
 
 enum LEVEL
 {
-    low,
-    normal,
-    high,
-    extra,
-    mega,
-    super_mega,
-    ultra_ultimate
+    bad = -15,
+    hate = -5,
+    none = 0,
+    low = 10,
+    normal = 25,
+    high = 40,
+    extra = 60,
+    mega = 65,
+    super_mega = 85,
+    ultra_ultimate = 100
 };
 
-struct User {
+struct User
+{
     char name[120];
     long int love_score = 0;
+    string loveStatus;
 };
 
+void loveStatus(User &user)
+{
+    long int score = user.love_score;
+
+    // Handle negative scores
+    if (score <= hate)
+    {
+        user.loveStatus = "Hate";
+        return;
+    }
+    else if (score < low)
+    {
+        user.loveStatus = "No Love";
+        return;
+    }
+
+    // Handle positive scores
+    if (score <= normal)
+    {
+        user.loveStatus = "Friend";
+        return;
+    }
+    else if (score <= high)
+    {
+        user.loveStatus = "Best friend";
+        return;
+    }
+    else if (score <= extra)
+    {
+        user.loveStatus = "Love";
+        return;
+    }
+    else if (score <= mega)
+    {
+        user.loveStatus = "Deep love";
+        return;
+    }
+    else if (score <= super_mega)
+    {
+        user.loveStatus = "Crazy love";
+        return;
+    }
+    else
+    {
+        user.loveStatus = "Eternal love";
+        return;
+    }
+}
 
 int main()
 {
@@ -30,21 +83,30 @@ int main()
     Tam = {"Tam"};
 
     User listUsers[] = {Kien, Tam};
+    while (true)
+    {
+        for (User user : listUsers)
+        {
+            loveStatus(user);
+            cout << user.name << ": " << user.loveStatus << endl;
+        };
 
-    while(true){
         bool round = rand() % 2;
         char answer;
-        cout << listUsers[round].name <<" do you love " << listUsers[!round].name << "? (y/n)" << endl;
+        cout << listUsers[round].name << " do you love " << listUsers[!round].name << "? (y/n)" << endl;
         cin.get(answer);
         cin.ignore(256, '\n');
-        
-        if(static_cast<char>(tolower(answer)) == 'y'){
+
+        if (static_cast<char>(tolower(answer)) == 'y')
+        {
             listUsers[!round].love_score += 5;
-            cout << "YES" << endl;
-        } else {
-            listUsers[!round].love_score -= 5;
-            cout << "NO" << endl;
         }
+        else
+        {
+            listUsers[!round].love_score -= 5;
+        }
+
+        system("cls");
     }
 
     return 0;
